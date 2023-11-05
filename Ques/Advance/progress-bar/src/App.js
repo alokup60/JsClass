7a522e9e1500b3e94be2966402df46e8d2e9c9cb
alokup60 <alokrajsngh@gmail.com>
2023-11-05 20:07:45 +0530

    progress bar design completed

--- Ques/Advance/progress-bar/src/App.js
+++ Ques/Advance/progress-bar/src/App.js
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import ProgressBar from "./components/ProgressBar";
 
 function App() {
-  return <div>App</div>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setValue((val) => val + 1);
+    }, 100);
+  }, []);
+
+  return (
+    <div className="main">
+      <h3>Progress Bar</h3>
+      <ProgressBar value={value} />
+    </div>
+  );
 }
 
 export default App;
--- Ques/Advance/progress-bar/src/components/ProgressBar.jsx
+++ Ques/Advance/progress-bar/src/components/ProgressBar.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./ProgressBar.css";
 
-const ProgressBar = () => {
-  return <div></div>;
+const ProgressBar = ({ value = 0 }) => {
+  const [percentage, setPercentage] = useState(value);
+
+  useEffect(() => {
+    setPercentage(Math.min(100, Math.max(value, 0)));
+  }, [value]);
+
+  return (
+    <div className="progress">
+      <span style={{ color: percentage > 49 ? "white" : "black" }}>
+        {percentage.toFixed()}%
+      </span>
+      <div style={{ width: `${percentage}%` }} />
+    </div>
+  );
 };
 
 export default ProgressBar;
